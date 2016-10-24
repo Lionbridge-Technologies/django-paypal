f75ab0ec1c7f254071b2e43c596d23c8eda4c59f
Joseph Lafuente <josephlafuente@gmail.com>
2016-10-25 02:07:37 +0800

    Added merge migration

---
+++ paypal/standard/ipn/migrations/0008_merge.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('ipn', '0002_paypalipn_eu_id'),
+        ('ipn', '0007_auto_20160219_1135'),
+    ]
+
+    operations = [
+    ]
